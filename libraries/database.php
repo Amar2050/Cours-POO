c866040bc214c484b7433bea92b8bae8b00dbeb8
Amar2050 <amarboutayeb@gmail.com>
2019-10-15 15:25:07 +0200

    Step 9 Singleton on database connexion

--- libraries/database.php
+++ libraries/database.php
@@ -2,6 +2,8 @@
 
 class Database {
 
+    private static $instance = null;
+
     /**
      * Return database connexion
      *
@@ -9,11 +11,15 @@ class Database {
      */
     public static function getPdo(): PDO {
 
+        if (self::$instance === null) 
+        {
+            self::$instance =
             $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            ]);
-            return $pdo;
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
         }
+        
+        return self::$instance;
+    }
         
 }

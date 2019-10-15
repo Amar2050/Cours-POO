1dcc112d41143b20806d362d94afb00177508ba9
Amar2050 <amarboutayeb@gmail.com>
2019-10-15 10:50:22 +0200

    Step 5 Namespace and controllers

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
 
-require_once('libraries/database.php');
-require_once('libraries/utils.php');
-require_once('libraries/models/Article.php');
+require_once('libraries/controllers/Article.php');
 
-$model = new Article();
-/**
- * 2. Récupération des articles
- */
-$articles = $model->findAll("created_at DESC");
-
-/**
- * 3. Affichage
- */
-$pageTitle = "Accueil";
-
-render('articles/index', compact('pageTitle', 'articles'));
+$controller = new \Controllers\Article();
+$controller->index();
